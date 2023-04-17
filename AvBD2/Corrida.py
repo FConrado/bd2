ceeb48c15e9b9d9ba12ee6ada9b7ba23a12f54a0
Felipe Conrado <66187989+FConrado@users.noreply.github.com>
2023-04-17 20:13:07 -0300

    Add files via upload

---
+++ AvBD2/Corrida.py
@@ -0,0 +1,8 @@
+import Passageiro
+
+class Corrida:
+    def __init__(self, nota_corrida: int, distancia: float, valor: float, passageiro: Passageiro):
+        self.nota_corrida = nota_corrida
+        self.distancia = distancia
+        self.valor = valor
+        self.passageiro = passageiro
---
+++ AvBD2/Motorista.py
@@ -0,0 +1,4 @@
+class Motorista:
+    def __init__(self, nota_motorista: int, corridas: tuple):
+        self.nota_motorista = nota_motorista
+        self.corridas = corridas
